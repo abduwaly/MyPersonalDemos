b0f5c2910d123839836033b24aafacc4df419fc4
arsay <arsay@foxmail.com>
2017-12-15 12:04:39 +0800

    ES6 learning/demo2.js (Promise,then)

--- ES6 learning/demo2.js
+++ ES6 learning/demo2.js
@@ -99,3 +99,55 @@ promise.then(function() {
 });
 console.log('outer');
 
+function getSum(n1, n2) {   
+   var isAnyNegative = function() {   
+      return n1 < 0 || n2 < 0;   
+   }   
+   var promise = new Promise(function(resolve, reject) {   
+      if (isAnyNegative()) {   
+         reject(Error("Negatives not supported"));   
+      }   
+      resolve(n1 + n2)
+   });   
+   return promise;   
+} 
+getSum(5, 6) .then(function (result) {   
+   console.log(result);   
+},function (error) {   
+   console.log(error);   
+});
+
+getSum(5, 6).then(function(result) {   
+   console.log(result);   
+   return getSum(10,20); 
+   // this returns another promise   
+},function(error) {   
+   console.log(error);   
+}).then(function(result) {   
+   console.log(result);   
+},function(error) {   
+   console.log(error);
+});    
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+getSum(5, 6).then(function(result) {   
+      console.log(result);   
+      return getSum(10, 20); 
+      //this returns another Promise   
+   },function(error) {   
+      console.log(error);   
+   }).then(function(result) {   
+      console.log(result);   
+      return getSum(30, 40); 
+      //this returns another Promise   
+   },function(error) {   
+      console.log(error);   
+   }).then(function(result) {   
+      console.log(result);   
+   },function(error) {         
+      console.log(error);   
+   });   
+   console.log("End of script ");   
+   
+console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
